8d6e93d3854346356c69cd136b88907dc2c9dfe5
rLuCy <clayton.geraghty413@gmail.com>
2019-06-24 16:08:48 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Discordify an image
+def discordify(image):
+  pass
+
+# Create original work
+def original(category):
+  if category == 'fanart':
+    pass
+  elif category == 'merch':
+    pass
+  elif category == 'emoji':
+    pass
+  elif category == 'story':
+    pass
