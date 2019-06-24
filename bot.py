45fd9567b935e298c6ce0295e4a2af69da6ee688
rLuCy <clayton.geraghty413@gmail.com>
2019-06-24 15:57:30 -0500

    Added message comprehension framework
    commands start with '+' and categories are fanart, emoji, merch, and story

--- bot.py
+++ bot.py
@@ -18,4 +18,18 @@ async def on_connect():
 async def on_disconnect():
     print("Bot has lost connection to Discord servers!")
 
+@client.event
+async def on_message(msg):
+    msgList = list(msg)
+    if msgList[0] == '+':
+        if msg.startswith('+fanart'):
+            pass
+        elif msg.startswith('+emoji'):
+            pass
+        elif msg.startswith('+merch'):
+            pass
+        elif msg.startswith('+story'):
+            pass
+        
+
 client.run("TOKEN")
